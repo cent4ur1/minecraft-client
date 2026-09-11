08fd65892a1ede9fdd752cd262bd1321be340da7
Lucas Papadam <lucaspapadam3@gmail.com>
2026-09-12 00:38:15 +1000

    updates

--- src/main/java/com/rottenapple/module/DescriptionSetting.java
+++
@@ -1,8 +0,0 @@
-package com.rottenapple.module;
-
-/** Static text row (mirrors Raven's DescriptionSetting). */
-public class DescriptionSetting extends Setting {
-    public DescriptionSetting(String t) {
-        super(t);
-    }
-}
--- src/main/java/com/rottenapple/module/GroupSetting.java
+++
@@ -1,8 +0,0 @@
-package com.rottenapple.module;
-
-/** Section header grouping settings (mirrors Raven's GroupSetting). */
-public class GroupSetting extends Setting {
-    public GroupSetting(String name) {
-        super(name);
-    }
-}
